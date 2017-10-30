99266339035b798ab66e3b8e0ce7b90547066b1f
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-30 13:20:27 -0500

    Added skeleton for testing framework

---
+++ Tests.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: Zane J Cersovsky
+ * Date: Oct 30 2017
+ * Tests the provided LinkedListOfInts
+ */
+#include "Tests.h"
+
+/*
+ * Runs all test cases
+ */
+void Tests::all(void)
+{
+
+}
+Tests::Tests(void) {}
---
+++ Tests.h
@@ -0,0 +1,16 @@
+/*
+ * Author: Zane J Cersovsky
+ * Date: Oct 30 2017
+ * Tests the provided LinkedListOfInts
+ */
+#pragma once
+
+class Tests {
+public:
+    void all(void);
+protected:
+    //List test cases here
+private:
+    //Disallow creating an instance of Tests
+    Tests(void);
+};
